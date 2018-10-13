9c1d2bc24c090b2987e410262558565777b1b565
Andrew Cotterell <ancotterell@The-Jetsons-MBP.hsd1.fl.comcast.net>
2018-10-13 10:11:18 -0400

    react-table

--- Web/twisted-management/src/scenes/Assistants/Assistants.jsx
+++ Web/twisted-management/src/scenes/Assistants/Assistants.jsx
@@ -22,107 +22,94 @@ import CardIcon from "components/Card/CardIcon.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
 
 import extendedTablesStyle from "assets/jss/material-dashboard-react/views/extendedTablesStyle.jsx";
+import "react-table/react-table.css";
 
 
 class Assistants extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          checked: []
-        };
-        this.handleToggle = this.handleToggle.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: []
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle(value) {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
     }
 
-    handleToggle(value) {
-      const { checked } = this.state;
-      const currentIndex = checked.indexOf(value);
-      const newChecked = [...checked];
-  
-      if (currentIndex === -1) {
-        newChecked.push(value);
-      } else {
-        newChecked.splice(currentIndex, 1);
+    this.setState({
+      checked: newChecked
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    const fillButtons = [
+      { color: "info", icon: Person },
+      { color: "success", icon: Edit },
+      { color: "danger", icon: Close }
+    ].map((prop, key) => {
+      return (
+        <Button color={prop.color} className={classes.actionButton} key={key}>
+          <prop.icon className={classes.icon} />
+        </Button>
+      );
+    });
+    const data = [{
+      name: 'Tanner Linsley',
+      age: 26,
+      friend: {
+        name: 'Jason Maurer',
+        age: 23,
       }
+    }]
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name' // String-based value accessors!
+    }, {
+      Header: 'Age',
+      accessor: 'age',
+      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    }, {
+      id: 'friendName', // Required because our accessor is not a string
+      Header: 'Friend Name',
+      accessor: d => d.friend.name // Custom value accessors!
+    }, {
+      Header: props => <span>Friend Age</span>, // Custom header components!
+      accessor: 'friend.age'
+    }]
   
-      this.setState({
-        checked: newChecked
-      });
-    }
-    
-
-    
-
-    render() {
-      
-        const { classes } = this.props;
-      
-        const fillButtons = [
-          { color: "info", icon: Person },
-          { color: "success", icon: Edit },
-          { color: "danger", icon: Close }
-        ].map((prop, key) => {
-          return (
-            <Button color={prop.color} className={classes.actionButton} key={key}>
-              <prop.icon className={classes.icon} />
-            </Button>
-          );
-        });
-
-        return (
-            <GridContainer>
-            <GridItem xs={12}>
-              <Card>
-                <CardHeader color="primary" icon>
-                  <CardIcon color="primary">
-                    <Assignment />
-                  </CardIcon>
-                  <h4 className={classes.cardIconTitle}>Assistants</h4>
-                </CardHeader>
-                <CardBody>
-              <Table
-                tableHead={[
-                  "Name",
-                  "Start Date",
-                  "# Clients",
-                  "Total Earnings",
-                  "Actions"
-                ]}
-                tableData={[
-                  [
-                    "Andrew Mike",
-                    "2018-10-12",
-                    "3",
-                    "$1200",
-                    fillButtons
-                  ],
-                  [
-                    "Andrew Mike",
-                    "2018-10-12",
-                    "3",
-                    "$1200",
-                    fillButtons
-                  ]
-                  
-                ]}
-                customCellClasses={[
-                  classes.left,
-                  classes.right,
-                  classes.right
-                ]}
-                customClassesForCells={[0, 4, 5]}
-                customHeadCellClasses={[
-                  classes.left,
-                  classes.right,
-                  classes.right
-                ]}
-                customHeadClassesForCells={[0, 4, 5]}
+    return (
+      <GridContainer>
+        <GridItem xs={12}>
+          <Card>
+            <CardHeader color="primary" icon>
+              <CardIcon color="primary">
+                <Assignment />
+              </CardIcon>
+              <h4 className={classes.cardIconTitle}>Assistants</h4>
+            </CardHeader>
+            <CardBody>
+              <ReactTable
+                data={data}
+                columns={columns}
+                className="-striped -highlight"
               />
             </CardBody>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        );
-    }
+          </Card>
+        </GridItem>
+      </GridContainer>
+    );
+  }
 }
 
 export default withStyles(extendedTablesStyle)(Assistants);
